d2864c4708ff1d602332c8f1153320e9a7f3c827
sergey-dubinin-work <sergey.dubinin.work@gmail.com>
2025-08-01 16:52:17 +0300

    first test and steps

---
+++ src/test/java/sergey/dubinin/steps/LoginSteps.java
@@ -0,0 +1,46 @@
+package sergey.dubinin.steps;
+
+import io.cucumber.java.ru.Дано;
+import io.cucumber.java.ru.И;
+import io.cucumber.java.ru.Когда;
+import io.cucumber.java.ru.Тогда;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LoginSteps {
+
+    private String username;
+    private String password;
+    private String resultMessage;
+
+    @Дано("пользователь открывает страницу входа")
+    public void openLoginPage(){
+        System.out.println("> > > Открыта страница входа");
+    }
+
+    @Когда("он вводит логин {string} и пароль {string}")
+    public void inputUsernameAndPassword(String username, String password){
+        System.out.printf("> > > Введён логин: %s\n", username);
+        System.out.printf("> > > Введён пароль: %s\n", password);
+        this.username = username;
+        this.password = password;
+    }
+
+    @И("нажимает на кнопку {string}")
+    public void clickOnButtonNamed(String buttonName){
+        System.out.printf("> > > Нажата кнопка: %s\n", buttonName);
+        if ("user".equals(username) && "pass123".equals(password)){
+            resultMessage = "Добро пожаловать!";
+        } else {
+            resultMessage = "Ошибка авторизации";
+        }
+    }
+
+    @Тогда("он должен увидеть сообщение {string}")
+    public void checkMessage(String message){
+        System.out.printf("> > > Ожидаемое сообщение: %s\n", resultMessage);
+        System.out.printf("> > > Фактическое сообщение: %s\n", message);
+        assertThat(message).isEqualTo(resultMessage);
+    }
+
+}
